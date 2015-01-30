2ed53e4f4ba9f88e1fa03390773844f16f6e7e4b
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-30 09:08:18 +0100

    Init and config from object pattern

--- database/config.py
+++ database/config.py
@@ -1,6 +1,18 @@
 """
 Config settings for the database.
 """
+def from_object(updates):
+    "Update same name (or prefixed) settings."
+    import sys
+    config = sys.modules[__name__]
+
+    prefix = config.__name__.split('.')[0].upper()
+    keys = [k for k in config.__dict__ if not k.startswith('_')]
+    get_value = lambda c, k: hasattr(c, k) and getattr(c, k) or None
+    for key in keys:
+        prefix_key = '%s_%s' % (prefix, key)
+        value = get_value(updates, prefix_key) or get_value(updates, key)
+        if value: setattr(config, key, value)
 
 ## SQLAlchemy
 ## http://docs.sqlalchemy.org/en/rel_0_9/core/engines.html
