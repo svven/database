c26a5b6fc830e9b8135b3f5190d987416f81e5a0
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-31 18:40:52 +0100

    Scoped session and model query
    Just like in Flask SQLAlchemy

--- database/config.py
+++ database/config.py
@@ -5,9 +5,10 @@ def from_object(updates):
     "Update same name (or prefixed) settings."
     import sys
     config = sys.modules[__name__]
-
+    
     prefix = config.__name__.split('.')[0].upper()
-    keys = [k for k in config.__dict__ if not k.startswith('_')]
+    keys = [k for k in config.__dict__ if \
+        k != from_object.__name__ and not k.startswith('_')]
     get_value = lambda c, k: hasattr(c, k) and getattr(c, k) or None
     for key in keys:
         prefix_key = '%s_%s' % (prefix, key)
--- database/db.py
+++ database/db.py
@@ -5,29 +5,46 @@ Similar to SQLAlchemy class from flask.ext.sqlalchemy.
 from . import config
 
 from sqlalchemy import \
-	create_engine, engine_from_config, func, event, \
+    create_engine, engine_from_config, func, event, \
     Column, ForeignKey, UniqueConstraint, CheckConstraint, \
     Boolean, DateTime, Enum, SmallInteger, Integer, BigInteger, String
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 
-Model = declarative_base()
-
 prefix = 'sqlalchemy_'
 
+## Engine
 get_engine = lambda config: engine_from_config(
     { key: getattr(config, key) for key in config.__dict__ if key.startswith(prefix) }, 
     prefix=prefix)
-get_session = sessionmaker()
 
+## Session
 def Session():
     engine = get_engine(config)
-    get_session.configure(bind=engine)
+    get_session = sessionmaker(bind=engine, autoflush=False)
     return get_session()
 
-# Session = lambda: sessionmaker(bind=get_engine(config))()
-# session = None # Session()
+session = scoped_session(Session)
 
-create_all = lambda: Model.metadata.create_all(get_engine(config))
-drop_all = lambda: Model.metadata.drop_all(get_engine(config))
+def init_app(app):
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        print "Remove session."
+        session.remove()
+
+## Model
+Model = declarative_base()
+Model.query = session.query_property()
+
+## Create and drop all
+def import_all():
+    import auth, news, twitter
+
+def create_all():
+    import_all()
+    Model.metadata.create_all(get_engine(config))
+
+def drop_all():
+    import_all()
+    Model.metadata.drop_all(get_engine(config))
